e80e3ff3d691995fcd22cbe7545d5d67e4cdfe61
JosephAaronMusara <josephaaronmusara@gmail.com>
2024-11-14 14:40:44 +0200

    account receivables

--- financeDashboard/bankBalances/models.py
+++ financeDashboard/bankBalances/models.py
@@ -24,3 +24,20 @@ class UnutilizedGrant(models.Model):
 class Total(models.Model):#not sure if its necessary since we are going to do the calculations on the FrontEnd
     description = models.CharField(max_length=50)
     total_amount = models.DecimalField(max_digits=20, decimal_places=2)
+
+class AccountReceivable(models.Model):
+    invoice_date = models.DateField()
+    invoice_number = models.CharField(max_length=10)
+    customer_name = models.CharField(max_length=100)
+    total_amount = models.DecimalField(max_digits=15, decimal_places=2)
+    due_date = models.DateField()
+    balance = models.DecimalField(max_digits=15, decimal_places=2)
+    service_type = models.CharField(max_length=50)
+    percentage = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)  #percentage as a decimal then we will get the % from FrontEnd
+    class Meta:
+        verbose_name = "Account Receivable"
+        verbose_name_plural = "Accounts Receivable"
+
+    def __str__(self):
+        return f"Invoice {self.invoice_number} - {self.customer_name}"
+
